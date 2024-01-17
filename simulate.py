4cd85214f40105c0393f8f740cb5345cbfd7fc76
Casey <cforey@uvm.edu>
2024-01-16 20:53:39 -0500

    Added Loop and sleep

--- simulate.py
+++ simulate.py
@@ -1,5 +1,11 @@
 import pybullet as p
-
+import time as t
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+for n in range(1,1001):
+    p.stepSimulation()
+    print(n)
+    t.sleep(1/60)
+
 
 p.disconnect()
