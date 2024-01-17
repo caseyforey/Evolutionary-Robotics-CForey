82fb2f4bb8ba899460b1a4ed7b3960461e272616
Casey <cforey@uvm.edu>
2024-01-17 09:27:36 -0500

    Added links

--- simulate.py
+++ simulate.py
@@ -1,7 +1,7 @@
 import pybullet as p
 import time as t
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+p.loadSDF("box.sdf")
 for n in range(1,1001):
     p.stepSimulation()
     print(n)
