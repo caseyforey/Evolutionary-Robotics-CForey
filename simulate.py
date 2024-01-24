ec3288b7d8659bcf67d15b203c9e66c01ab4bbd5
Casey <cforey@uvm.edu>
2024-01-24 11:01:54 -0500

    Finished Step 8

--- simulate.py
+++ simulate.py
@@ -1,14 +1,17 @@
 import pybullet as p
 import time as t
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
 for n in range(1,1001):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     t.sleep(1/60)
     print(n)
 
