eccf8a1604e9a07d6eaa4a7269cbd1cb9c4f824b
Casey <cforey@uvm.edu>
2024-02-12 17:39:33 -0500

    Completed Motors

--- analyze.py
+++ analyze.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as mpl
 backLegSensorValues = np.load('data/backleg.npy')
 frontLegSensorValues = np.load('data/frontleg.npy')
 
-mpl.plot(backLegSensorValues, label = 'Back Leg', linewidth = 1.5)
-mpl.plot(frontLegSensorValues, label = 'Front Leg' , linewidth = 0.75)
+backLegAngles = np.load('data/backlegAngles.npy')
+
+frontLegAngles = np.load('data/frontlegAngles.npy')
+
+#mpl.plot(backLegSensorValues, label = 'Back Leg', linewidth = 1.5)
+#mpl.plot(frontLegSensorValues, label = 'Front Leg' , linewidth = 0.75)
+mpl.plot(backLegAngles, label = 'back leg' , linewidth = 2)
+mpl.plot(frontLegAngles, label = 'front leg' , linewidth = 0.75)
 
 mpl.legend(loc = 1)
 mpl.show()
--- simulate.py
+++ simulate.py
@@ -6,6 +6,7 @@ import numpy as np
 import random as rand
 # Connect to client
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 # Set gravity
@@ -22,17 +23,42 @@ p.loadSDF("world.sdf")
 
 # Simulate the robot
 pyrosim.Prepare_To_Simulate(robotId)
-
+ITERATIONS = 1000
 # Create list for back leg data
-backLegSensorValues = np.zeros(1000)
-frontLegSensorValues = np.zeros(1000)
+backLegSensorValues = np.zeros(ITERATIONS)
+frontLegSensorValues = np.zeros(ITERATIONS)
+
+# Array of range 0 to 1000
+pi = np.pi
+# Create 1000 values from -pi to pi 
+vals = np.linspace(-pi,pi,ITERATIONS) 
+
+# Back Leg 
+
+# back leg specific vars
+backLegAmplitude = pi/4
+backLegFrequency = 15
+backLegPhaseOffset = 0
+# Sin scaled to pi/4
+backLegTargetAngles = backLegAmplitude * np.sin(backLegFrequency * vals + backLegPhaseOffset)
+
+
+# Front Leg
+
+# front leg specific vars
+frontLegAmplitude = pi/4
+frontLegFrequency = 8
+frontLegPhaseOffset = pi
+# Sin scaled to pi/4
+frontLegTargetAngles = frontLegAmplitude * np.sin(frontLegFrequency * vals + frontLegPhaseOffset)
+
 
 # Basic Engine loop
-for n in range(1000):
+for n in range(ITERATIONS):
     p.stepSimulation()
     backLegSensorValues[n] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[n] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    
+
     # Back leg motor
     pyrosim.Set_Motor_For_Joint(
 
@@ -42,10 +68,9 @@ for n in range(1000):
 
     controlMode = p.POSITION_CONTROL,
 
-    targetPosition = (np.pi * rand.random()) - (np.pi/2),
+    targetPosition = backLegTargetAngles[n],
 
-    maxForce = 500)
-
+    maxForce = 20)
 
     # Front leg motor
     pyrosim.Set_Motor_For_Joint(
@@ -56,15 +81,17 @@ for n in range(1000):
 
     controlMode = p.POSITION_CONTROL,
 
-    targetPosition =  (np.pi * rand.random()) - (np.pi/2),
+    targetPosition =  frontLegTargetAngles[n],
 
-    maxForce = 500)
+    maxForce = 20)
 
 
 
 
     t.sleep(1/60)
 
+np.save('data/backlegAngles',backLegTargetAngles)
+np.save('data/frontlegAngles',frontLegTargetAngles)
 np.save('data/backleg',backLegSensorValues)
 np.save('data/frontleg',frontLegSensorValues)
 p.disconnect()
